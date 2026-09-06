ca92a40ea15a95ae813cff35095d203a72f0f0a5
umar abbas <umarshah6444@gmail.com>
2026-09-06 08:00:00 +0500

    middlwares update

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const middleWares = (app)=>{
 
-    console.log("middleware working.")
+    // console.log("middleware working.")
 
     app.use(express.json());
+    app.use(cookieParser())
     app.use(express.urlencoded({extended:true}))
 
 
