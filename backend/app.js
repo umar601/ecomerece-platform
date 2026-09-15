56a8a4d9e1c224f8a336a3c1222135940029dbaa
umar abbas <umarshah6444@gmail.com>
2026-09-15 06:10:00 +0500

    app

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,36 @@
 const express = require("express");
-
 const app = express();
 
-const middleware = require("./middlewares/middleware");
+const middleware = require("./middlewares/main.middleware");
+const errorMiddleware = require("./middlewares/error.middleware");
+
+const userRouter = require("./routes/user.route");
+const productRouter = require("./routes/product.route");
+const reviewRouter = require("./routes/review.route");
 
 //middleware
 
 middleware(app);
 
 //routes
-app.use("/", (req, res) => {
-  res.send("hello backend");
+
+//admin routes
+app.use("/admin", userRouter);
+
+//product routes
+app.use("/product", productRouter);
+
+//review routes
+
+app.use("/review", reviewRouter);
+
+app.use("/", async (req, res) => {
+  res.send("404 page not found");
 });
+
+//error middelware
+
+errorMiddleware(app);
 
 //export
 module.exports = app;
