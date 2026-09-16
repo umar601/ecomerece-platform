bb9677b1b78bb6841659766749dc5de0e119f40a
umar abbas <umarshah6444@gmail.com>
2026-09-17 02:10:00 +0500

    server

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const app = require("./app");
 const port = process.env.PORT || 8080;
-const url = process.env.URL || "mongodb://localhost:27017/ecommerce";
+const url = process.env.URL || "mongodb://localhost:27017/ecommerceapp";
+const {redisConnection} = require("./config/redis.connection");
 
-const connection = require("./connections/database.connection");
+const connection = require("./config/database.connection");
 
 //data base connection
 connection(url);
+redisConnection();
 
 // running server
 app.listen(port, () => {
